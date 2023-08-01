d98a9b91c7580044bf97f325031acdad8fd3d6b3
= <kirklimushin@gmail.com>
2023-08-01 16:49:16 +0300

    Updated Data Drift and added Initial Training Notebook for baseline models

---
+++ experiments/experiment_1/encoders/encoders.py
@@ -0,0 +1,10 @@
+from ....src.encoders import encoders 
+import pandas 
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, TargetEncoder
+
+class DatasetEncoder(encoders.BaseDatasetEncoder):
+    
+    def encode_dataset(self, dataset: pandas.DataFrame) -> pandas.DataFrame:
+        """
+        """
+        pass
---
+++ experiments/experiment_1/features/build_features.py
@@ -0,0 +1,6 @@
+import pandas 
+import numpy 
+
+def build_features(dataset: pandas.DataFrame) -> pandas.DataFrame:
+    """
+    """
