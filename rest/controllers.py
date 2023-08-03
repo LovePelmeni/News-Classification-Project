f5d554ae05cc41223462c1ad0f8c4976b93f653d
= <kirklimushin@gmail.com>
2023-08-03 11:01:44 +0300

    Updated rest controllers, added reports section

--- rest/controllers.py
+++ rest/controllers.py
@@ -1,7 +1,7 @@
 import logging 
 from fastapi.responses import JSONResponse
 from fastapi.requests import Request
-import definitions 
+import definitions
 
 rest_logger = logging.getLogger(__name__)
 
